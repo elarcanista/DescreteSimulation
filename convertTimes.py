a4ec88d5ccff023d79c441b4555156ffa9c12cf5
Andrés Ortega <elarcanista@gmail.com>
2018-05-23 11:51:32 -0500

    compressed all arrays into single entries

--- convertTimes.py
+++ convertTimes.py
@@ -25,19 +25,35 @@ def str2Time(strTime):
 
 def parse(line, dateFields=[], timeFields=[], refDate=dt.datetime(1,1,1)):
     fields = re.findall(r"[^|\n]+",line)
+    fields[0] = re.match(r"(\d+)(?:_\d+)?", fields[0]).groups()[0]
     for i in dateFields:
         fields[i] = date2TimeSince(str2Date(fields[i]), refDate)
     for i in timeFields:
         fields[i] = str2Time(fields[i])
     return fields
 
+def insert2Dict(d, item):
+    key = item[0]
+    if key in d:
+        prevVal = d[key]
+        d[key][5] = min(prevVal[5], item[5]) #submission
+        d[key][6] = min(prevVal[6], item[6]) #start
+        d[key][7] = prevVal[7] + item[7]     #elapsed
+        d[key][8] = max(prevVal[8], item[8]) #end
+    else:
+        d[key] = item
+
 if __name__ == "__main__":
+    uniqueProcess = {}
     for i in range(1,len(sys.argv)):
         inF = open(sys.argv[i], "r")
-        outF = open(os.path.splitext(sys.argv[i])[0]+'.out', 'w')
-        inF.readline()
+        outF = open(os.path.splitext(sys.argv[i])[0]+'.out1', 'w')
+        headers = re.findall(r"[^|\n]+",inF.readline())
+        print(*headers, sep = '\t', file = outF)
         for line in inF:
             parsedLine = parse(line, [5,6,8], [4,7], dt.datetime(2018,1,1))
-            print(*parsedLine, sep='\t', file=outF)
+            insert2Dict(uniqueProcess, parsedLine)
+        for key, item in uniqueProcess.items():
+            print(*item, sep='\t', file=outF)
         inF.close()
         outF.close()
